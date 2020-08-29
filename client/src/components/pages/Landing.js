aef2ff0d72e3629156ed819cc7bc24f8e0b82b1c
john katua <johnkatua99@gmail.com>
2020-08-29 16:32:32 +0300

    first commit

---
+++ client/src/components/pages/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Landing = () => (
+    <div>
+      <h1>Selling you great stuffs!</h1>
+      <p>With the help of a chatbot</p>
+    </div>
+  )
+
+
+export default Landing;
--- routes/dialogFlow.js
+++ routes/dialogFlow.js
@@ -5,13 +5,21 @@ module.exports = app => {
     res.send({'Hello': 'John'});
   });
 
-  app.post('/api/df_text_query', async (req, res) => { 
-    let responses = await chatbot.textQuery(req.body.text, req.body.parameters);  
+  app.post('/api/df_text_query', async (req, res) => {
+    console.log(req); 
+    let responses = await chatbot.textQuery(req.body.text, req.body.userID, req.body.parameters);  
     res.send(responses[0].queryResult);
   });
 
   app.post('/api/df_event_query', async(req, res) => {
-    let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);  
-    res.send(responses[0].queryResult);
+    console.log(req.body)
+    try {
+      let responses = await chatbot.eventQuery(req.body.event, req.body.userID, req.body.parameters);  
+      res.send(responses[0].queryResult);
+    } catch(error){
+      console.log(error)
+      res.status(400).json({message : 'eror occured'})
+    }
+
   });
 }
